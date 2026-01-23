b7be2514cb7a1dc8c6f20a0a6d7b928863414918
martinfroberg08 <frobergmartin292@gmail.com>
2026-01-23 09:16:59 +0000

    changed darkmode

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+
+const toggleBtn = document.getElementById('toggle');
+const body = document.body;
+
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  body.classList.add("dark-mode");
 }
 
-document.getElementById('toggle').addEventListener('click', toggleDarkMode);
 
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
 
 const yearSpan = document.querySelector('#currentYear');
 const currentYear = new Date().getFullYear();
